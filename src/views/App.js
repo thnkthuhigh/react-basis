3c811c883250bcf75241e6c743ad6fc1354e88f7
Thanh <a01204496068@gmail.com>
2024-02-02 16:50:42 +0700

    Component

--- src/views/App.js
+++ src/views/App.js
@@ -1,7 +1,7 @@
 import logo from "./logo.svg";
 import "./App.scss";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
       <header className="App-header">
@@ -18,6 +18,6 @@ function App() {
       </header>
     </div>
   );
-}
+};
 
 export default App;
